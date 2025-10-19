df1b81820c289184a0d52f806fe8f1acbe02ad8d
Varshanth2025 <varshanthg2030@gmail.com>
2025-10-19 21:40:05 +0530

    addedd ui to authentication

---
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -0,0 +1,171 @@
+"use client";
+import {z} from "zod"
+import {zodResolver} from "@hookform/resolvers/zod"
+import {OctagonAlertIcon } from "lucide-react";
+import { useForm} from "react-hook-form";
+import { Card, CardContent } from "@/components/ui/card";
+import { Alert,AlertTitle } from "@/components/ui/alert";
+import * as FormComponents from "@/components/ui/form";
+import { Form } from "@/components/ui/form";
+import { authClient } from "@/lib/auth-client";
+
+import {Input} from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+
+const formSchema=z.object({
+    name:z.string().min(1,{message:"Name is required"}),
+    email: z.string().email(),
+    password:z.string().min(1,{message:"Password is required"}),
+    confirmPassword:z.string().min(1,{message:"Password is required"})
+})
+.refine((data)=>data.password===data.confirmPassword,{
+    message:"Passwords don't match",
+    path:["confirmPassword"],
+});
+
+
+
+export const SignUpView=()=>{  
+    const router=useRouter();
+    const [Error,setError]=useState<string|null>(null);
+    const [Pending,setPending]=useState(false);
+    const form=useForm<z.infer<typeof formSchema>>({
+        resolver:zodResolver(formSchema),
+        defaultValues:{
+            name:"",
+            email:"",
+            password:"",
+            confirmPassword:"",
+        }
+    })
+const onSubmit=(data:z.infer<typeof formSchema>)=>{
+    setError(null);
+    setPending(true);
+    authClient.signUp.email(
+        {
+            name:data.name,
+            email:data.email,
+            password:data.password,
+        },
+        {
+            onSuccess:()=>{
+                setPending(false);
+                router.push("/");
+            },
+            onError:({error})=>{
+                setError(error.message);
+                setPending(false);
+
+            }  
+        }
+    );
+
+};
+
+
+    return (
+        <div className="flex  flex-col gap-6">
+ <Card className="overflow-hidden p-0 ">
+   <CardContent className="grid p-0 md:grid-cols-2">
+<Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
+        <div className="flex flex-col gap-6">
+            <div className="flex flex-col items-center text-center ">
+                <h1 className="text-2xl font-bold">Let&apos;s get started</h1>
+                <p className="text-muted-foreground text-balance">
+                    Create ur account
+                </p>
+            </div>
+            <div className="grid gap-3">
+                <FormComponents.FormField control={form.control} name="name" render={({field})=>(
+                    <FormComponents.FormItem>
+                        <FormComponents.FormLabel>Name</FormComponents.FormLabel>
+                        <FormComponents.FormControl>
+                            <Input type="text" placeholder="John Doe" {...field}/>
+                        </FormComponents.FormControl>
+                        <FormComponents.FormMessage/>
+                    </FormComponents.FormItem>
+                )}/>
+            </div>
+            <div className="grid gap-3">
+                <FormComponents.FormField control={form.control} name="email" render={({field})=>(
+                    <FormComponents.FormItem>
+                        <FormComponents.FormLabel>Email</FormComponents.FormLabel>
+                        <FormComponents.FormControl>
+                            <Input type="email" placeholder="m@example.com" {...field}/>
+                        </FormComponents.FormControl>
+                        <FormComponents.FormMessage/>
+                    </FormComponents.FormItem>
+                )}/>
+            </div>
+             <div className="grid gap-3">
+                <FormComponents.FormField control={form.control} name="password" render={({field})=>(
+                    <FormComponents.FormItem>
+                        <FormComponents.FormLabel>Password</FormComponents.FormLabel>
+                        <FormComponents.FormControl>
+                            <Input type="password" placeholder="********" {...field}/>
+                        </FormComponents.FormControl>
+                        <FormComponents.FormMessage/>
+                    </FormComponents.FormItem>
+                )}/>
+            </div>
+            <div className="grid gap-3">
+                <FormComponents.FormField control={form.control} name="confirmPassword" render={({field})=>(
+                    <FormComponents.FormItem>
+                        <FormComponents.FormLabel>Confirm Password</FormComponents.FormLabel>
+                        <FormComponents.FormControl>
+                            <Input type="password" placeholder="********" {...field}/>
+                        </FormComponents.FormControl>
+                        <FormComponents.FormMessage/>
+                    </FormComponents.FormItem>
+                )}/>
+            </div>
+{!!Error &&(
+    <Alert className="bg-destructive/10 border-none">
+        <OctagonAlertIcon className="h-4 w-4 !text-destructive"/>
+        <AlertTitle>{Error}</AlertTitle>
+    </Alert>
+)}
+
+<Button type="submit" className="w-full" disabled={Pending}>
+
+    Sign up
+</Button>
+<div className="after:border-border  relative text-center text-sm 
+after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after-border-t">
+<span className="bg-card text-muted-foreground relative z-10 px-2">
+    Or continue with 
+</span >
+
+</div>
+<div className="grid grid-cols-2 gap-4">
+    <Button variant="outline" type="button" className="w-full" disabled={Pending}>
+        Google
+    </Button>
+    <Button variant="outline" type="button" className="w-full" disabled={Pending}>
+        Github
+    </Button>
+        </div>
+        <div className="text-center text-sm">
+            Already have an account?{" "} <Link href="/sign-in" className=" underline underline-offset-4 ">Sign in</Link>
+        </div>
+        </div>
+    </form>
+    </Form>
+
+    <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4  items-center justify-center ">
+        <img src="/logo.svg" alt="Image" className="h-[92px] w-[92px]" />
+        <p className="text-2xl font-semibold text-white">MockMeet</p>
+    </div>
+    </CardContent>
+   </Card>
+   <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+    By clicking continue ,you agree to our <a href="#" >Terms of Service</a> and <a href="#">Privacy Policy</a>
+   </div>
+ </div>
+    );
+};
