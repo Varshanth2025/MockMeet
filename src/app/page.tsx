a6610ed909ae0a1aa7544551a58213da765f882d
Varshanth2025 <varshanthg2030@gmail.com>
2025-10-19 13:05:42 +0530

    created authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,69 @@
+"use client";
+
+
+import { authClient } from "@/lib/auth-client"; 
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Page() {
-  return <Button>Click me!</Button>;
+   const [email,Setemail]=useState("");
+  const [name,Setname]=useState("");
+  const [password,Setpassword]=useState("");
+
+ const { data: session, 
+    } = authClient.useSession() 
+
+const onsubmit=()=>{
+authClient.signUp.email({
+        email, // user email address
+        password, // user password -> min 8 characters by default
+        name,
+},{
+  onError:()=>{
+    window.alert("something went wrong");
+  },
+  onSuccess:()=>{
+    window.alert("Success");
+  }
+});
+}
+const onlogin=()=>{
+authClient.signIn.email({
+        email, // user email address
+        password
+},{
+  onError:()=>{
+    window.alert("something went wrong");
+  },
+  onSuccess:()=>{
+    window.alert("Success");
+  }
+});
+}
+
+if(session){
+  return (<div className="flex flex-col p-4  gap-y-4">
+    <p>Logged in as {session.user.name}</p>
+    <Button onClick={()=>authClient.signOut()} >Sign out</Button>
+  </div>
+  );
+}
+
+
+ 
+  return <div className="flex flex-col gap-y-10">
+  <div className="p-4 flex flex-col gap-y-4">
+    
+    <input placeholder="name" value={name} onChange={(e)=>Setname(e.target.value)}/>
+    <input placeholder="email" value={email} onChange={(e)=>Setemail(e.target.value)} />
+    <input placeholder="password" type="password" value={password} onChange={(e)=>Setpassword(e.target.value)} />
+    <Button onClick={onsubmit}>Create User</Button>
+    </div>
+    <div className="p-4 flex flex-col gap-y-4">
+    <input placeholder="email" value={email} onChange={(e)=>Setemail(e.target.value)} />
+    <input placeholder="password" type="password" value={password} onChange={(e)=>Setpassword(e.target.value)} />
+    <Button onClick={onlogin}>Login</Button>
+    </div>
+
+  </div>
 }
