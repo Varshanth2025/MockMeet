a7207c002e5c857ee877337acce679b50ea1aff0
Varshanth2025 <varshanthg2030@gmail.com>
2025-10-26 19:55:24 +0530

    created-data table for meetings

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -3,13 +3,15 @@ import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { DataTable } from "../components/data-table";
+
 import { columns } from "../components/columns";
-import { EmptyState } from "@/components/empty-state";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { agents } from "@/db/schema";
 import { DataPagination } from "../components/data-pagination";
 import { useRouter } from "next/navigation";
+import { DataTable } from "@/components/data-table";
+import { EmptyState } from "./agents-empty-state";
+
 
 
 export const AgentsView=()=>{
